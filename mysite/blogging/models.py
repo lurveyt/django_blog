bbfff931cf2a5f742af562a7be6757b435a471b7
tim <lurveyt@uw.edu>
2021-02-08 22:06:50 -0800

    blogging setup

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import CharField, TextField, DateTimeField, ForeignKey
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = CharField(max_length=150)
+    text = TextField(blank=True)
+    author = ForeignKey(User, on_delete=models.CASCADE)
+    created_date = DateTimeField(auto_now_add=True)
+    modified_date = DateTimeField(auto_now=True)
+    published_date = DateTimeField(blank=True, null=True)
