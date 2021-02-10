205c5ed161dd485366017b55b3219ca1cacf8159
tim <lurveyt@uw.edu>
2021-02-09 23:28:26 -0800

    login

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+
+#TODO: read https://docs.djangoproject.com/en/2.1/topics/forms/modelforms/
