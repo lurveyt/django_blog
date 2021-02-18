edc9298a3985272627b41e2a4b51e676214f938d
tim <lurveyt@uw.edu>
2021-02-17 22:59:58 -0800

    error on azure trying to resolve SystemCheckError: System check identified some issues:
    ERRORS:
    ?: (staticfiles.E002) The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,14 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'blogging', 'static'),
+    os.path.join(BASE_DIR, 'polling', 'static'),
+    os.path.join(BASE_DIR, 'mysite', 'static'),
 ]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
-# TODO: read https://docs.djangoproject.com/en/2.1/topics/forms/modelforms/
